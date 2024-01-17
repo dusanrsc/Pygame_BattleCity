87533829a5252aca52da4cc73de8d972873a139f
Dusan Rosic <dusanrosic25.06.1997@gmail.com>
2024-01-17 17:30:38 +0100

    first commit

--- main.py
+++ main.py
@@ -52,6 +52,35 @@ enemy_fireing = 0
 # game lists
 # cpu spawn points
 enemy_spawn = [0, 350, 650]
+board = [
+[[1,  1], [1,  2], [1,  3], [1,  4], [1,  5], [1,  6], [1,  7], [1,  8], [1,  9], [1,  10], [1,  11], [1,  12], [1,  13]],
+[[2,  1], [2,  2], [2,  3], [2,  4], [2,  5], [2,  6], [2,  7], [2,  8], [2,  9], [2,  10], [2,  11], [2,  12], [2,  13]],
+[[3,  1], [3,  2], [3,  3], [3,  4], [3,  5], [3,  6], [3,  7], [3,  8], [3,  9], [3,  10], [3,  11], [3,  12], [3,  13]],
+[[4,  1], [4,  2], [4,  3], [4,  4], [4,  5], [4,  6], [4,  7], [4,  8], [4,  9], [4,  10], [4,  11], [4,  12], [4,  13]],
+[[5,  1], [5,  2], [5,  3], [5,  4], [5,  5], [5,  6], [5,  7], [5,  8], [5,  9], [5,  10], [5,  11], [5,  12], [5,  13]],
+[[6,  1], [6,  2], [6,  3], [6,  4], [6,  5], [6,  6], [6,  7], [6,  8], [6,  9], [6,  10], [6,  11], [6,  12], [6,  13]],
+[[7,  1], [7,  2], [7,  3], [7,  4], [7,  5], [7,  6], [7,  7], [7,  8], [7,  9], [7,  10], [7,  11], [7,  12], [7,  13]],
+[[8,  1], [8,  2], [8,  3], [8,  4], [8,  5], [8,  6], [8,  7], [8,  8], [8,  9], [8,  10], [8,  11], [8,  12], [8,  13]],
+[[9,  1], [9,  2], [9,  3], [9,  4], [9,  5], [9,  6], [9,  7], [9,  8], [9,  9], [9,  10], [9,  11], [9,  12], [9,  13]],
+[[10, 1], [10, 2], [10, 3], [10, 4], [10, 5], [10, 6], [10, 7], [10, 8], [10, 9], [10, 10], [10, 11], [10, 12], [10, 13]],
+[[11, 1], [11, 2], [11, 3], [11, 4], [11, 5], [11, 6], [11, 7], [11, 8], [11, 9], [11, 10], [11, 11], [11, 12], [11, 13]],
+[[12, 1], [12, 2], [12, 3], [12, 4], [12, 5], [12, 6], [12, 7], [12, 8], [12, 9], [12, 10], [12, 11], [12, 12], [12, 13]],
+[[13, 1], [13, 2], [13, 3], [13, 4], [13, 5], [13, 6], [13, 7], [13, 8], [13, 9], [13, 10], [13, 11], [13, 12], [13, 13]]]
+
+# level = [
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [1,  1], [1,  1], [1,  1], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]],
+# [[0,  0], [0,  0], [0,  0], [0,  0], [0,  0], [1,  1], [0,  0], [1,  1], [0,  0], [0,  0], [0,  0], [0,  0], [0,  0]]]
 
 # setting up screen
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -150,6 +179,23 @@ player_group.add(player)
 flag_group = pygame.sprite.Group()
 flag = Block(pos_x=7, pos_y=13, img="static/flag.png")
 flag_group.add(flag)
+
+brickwall_group = pygame.sprite.Group()
+# primitive level construction
+for item in board[1:12:]:
+	for index, value in item[1:12:]:
+		print(index, value)
+		brickwall = Block(pos_x=index, pos_y=value, img="static/brick_wall.png")
+		brickwall_group.add(brickwall)
+
+# for item in level:
+# 	for index, value in item:
+# 		if index == 1 and value == 1:
+# 			print(index, value)
+# 			brickwall = Block(pos_x=index, pos_y=value, img="static/brick_wall.png")
+# 			brickwall_group.add(brickwall)
+# 		else:
+# 			pass
 
 bullet_group = pygame.sprite.Group()
 bullet = Bullet(pos_x=player.rect.x, pos_y=player.rect.y)
@@ -310,22 +356,29 @@ while running:
 		else:
 			enemy.kill()
 
-	# if players bullet hit brickwall
+	# if players bullet hit flag
 	if pygame.sprite.groupcollide(bullet_group, flag_group, True, True, pygame.sprite.collide_rect_ratio(.85)):
 		exit()
+
+	# if players bullet hit brickwall
+	if pygame.sprite.groupcollide(bullet_group, brickwall_group, True, True, pygame.sprite.collide_rect_ratio(.85)):
+		bullet_counter = 0
 
 	# if player hit enemy
 	if pygame.sprite.groupcollide(player_group, enemy_group, False, False, pygame.sprite.collide_rect_ratio(.85)):
 		enemy_direction = random.randint(0, 3)
 		enemy_moving_range = random.randint(0, SCREEN_WIDTH)
 
-	# if enemy hit brickwall
+	# if enemy hit flag
 	if pygame.sprite.groupcollide(enemy_group, flag_group, False, False, pygame.sprite.collide_rect_ratio(.85)):
 		enemy_direction = random.randint(0, 3)
 		enemy_moving_range = random.randint(0, SCREEN_WIDTH)
 
 	# drawing sprites on the screen
 	SCREEN.fill(BLACK)
+
+	brickwall_group.update()
+	brickwall_group.draw(SCREEN)
 
 	bullet_group.update()
 	bullet_group.draw(SCREEN)
